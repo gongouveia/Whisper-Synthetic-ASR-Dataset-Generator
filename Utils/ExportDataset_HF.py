0e3fae0d9f0d6a0d071caab86902c831997271e9
Gonçalo Gouveia <68733294+gongouveia@users.noreply.github.com>
2024-03-27 14:51:22 +0000

    ExportDataset_HF.py, error handlings, create dataset, export and load

--- Utils/ExportDataset_HF.py
+++ Utils/ExportDataset_HF.py
@@ -1,14 +1,10 @@
-from datasets import load_dataset
 import csv
 import os
 import sys
+import pandas
+from datasets import load_dataset, load_from_disk,Audio, load_dataset
+from transformers import WhisperProcessor, WhisperFeatureExtractor, WhisperTokenizer
 
-from datasets import load_dataset, load_from_disk
-from datasets import Audio
-
-from transformers import WhisperProcessor
-from transformers import WhisperFeatureExtractor
-from transformers import WhisperTokenizer
 
 
 """
@@ -19,74 +15,97 @@ For now it is exploratory, and checkif fucntions work
 
 """
 
-
-
-def create_hf_audio_dataset(path_to_data):                                   #TODO verify | creates dataset
-    dataset = load_dataset("audiofolder", data_dir=path_to_data)
-    return dataset
-
-
 def dataset_validator(csv_path):                                             #TODO verify | verifyes wetehr the dataset is good to upload 
-    with open(csv_path) as f:
-        data = list(csv.reader(f))
-        
-    print(data)    
-    print("rows:", len(data))
-    print("columns:", len(data[0]))
-    return True
+    try:
+        with open(csv_path) as f:
+            data = list(csv.reader(f))
+        print("rows:", len(data))
+        print("columns:", len(data[0]))
+        data = pd.Dataframe(data)
+        return 1
+    except:
+        print('Dataframe format invalid, path does not exist').       
+        return None
+
+
+def create_hf_dataset(data_folder_path = "HuggingFaceDataset/",
+                      dataset_name = 'hf_audio_dataset.hf',
+                      sampling_rate =16000,
+                      model = 'openai/whisper-medium',
+                      language = 'en'):
+
+    try:
+        dataset = load_dataset("audiofolder", data_dir=data_folder_path)
+        if sampling_rate != None:  #resample audio to sampling_rate value, 16khz is best for whisper
+            dataset = dataset.cast_column("audio", Audio(sampling_rate=sampling_rate))
+    
+        feature_extractor = WhisperFeatureExtractor.from_pretrained(model)
+        tokenizer = WhisperTokenizer.from_pretrained(model, language=language, task="transcribe")
+    
+        def prepare_dataset(batch):
+            # load and resample audio data from 48 to 16kHz
+            audio = batch["audio"]
+    
+            # compute log-Mel input features from input audio array 
+            batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
+    
+            # encode target text to label ids 
+            batch["labels"] = tokenizer(batch["transcription"]).input_ids
+            return batch
+    
+        #REF: https://huggingface.co/docs/datasets/v2.15.0/en/package_reference/main_classes#datasets.Dataset.map
+        dataset = dataset.map(prepare_dataset, remove_columns=dataset.column_names["train"], num_proc=1)
+        dataset.save_to_disk(dataset_name)
+        return 1
+    except:
+        return None
+
+
 
 
 def export_dataset_to_HF_hub(dataset, hf_token, dataset_name='new_dataset'):                             #TODO verificar  | installs dependencies if necessary and logs in HF account to export
     
     if 'huggingface_hub' not in sys.modules: 
-        # os.system('pip install huggingface_hub')
-        os.system('pip install -U "huggingface_hub[cli]"')
-        print('here')
+        try:
+            # os.system('pip install huggingface_hub')
+            syss = os.system('pip install -U "huggingface_hub[cli]"')
+            print(syss)
+            return 1
+        except:
+            print('Could not install dependencies. Verify python version and internet connection. ')
+            return None
     else:
-        
-        login = os.system(f'huggingface-cli login --token {hf_token} --add-to-git-credential')
-        print(login)
-        repo_create = os.system(f'huggingface-cli repo create {dataset_name} --type dataset')
-        print(repo_create)
-        upload = os.system('huggingface-cli upload')                                                     #Verificar
-        print(upload)
-                  
+        try:
+            try:
+                login = os.system(f'huggingface-cli login --token {hf_token} --add-to-git-credential')
+                print(login)
+            except:
+                print('Action not completed')
+            try:
+                repo_create = os.system(f'huggingface-cli repo create {dataset_name} --type dataset')
+                print(repo_create)
+            except:
+                print('Action not completed')
+            try:
+                upload = os.system('huggingface-cli upload')  
+                print(upload)
+            except:
+                print('Action not completed')
+        except: 
+            return None
+
+def laod_model_from_disk(hf_dataset_name):           #DEBUG purpose, TESTED
+    try:
+        dataset =  load_from_disk(hf_dataset_name)
+        return dataset
+    except:
+        print('Could not load from disk, file may be non existent or reachable')
+        return None
 
 
-def feature_token_extraction(whisper_model = "openai/whisper-medium", language = 'en'):
-    feature_extractor = WhisperFeatureExtractor.from_pretrained(whisper_model)
-    tokenizer = WhisperTokenizer.from_pretrained(whisper_model, language=language, task="transcribe")
-    print('Preparing dataset for hugging face with tokenizer and feature extractor\n\n')
-
-    return feature_extractor, tokenizer
-
-
-def prepare_dataset(batch, feature_extractor, tokenizer):
-    # load and resample audio data from 48 to 16kHz
-    audio = batch["audio"]
-    batch["input_features"] = feature_extractor(audio["array"], sampling_rate=audio["sampling_rate"]).input_features[0]
-    batch["labels"] = tokenizer(batch["transcription"]).input_ids
-    print('dataset_prepared')
-    return batch
-
-# # https://huggingface.co/docs/datasets/v2.15.0/en/package_reference/main_classes#datasets.Dataset.map
-
-def prepare_and_store_dataset_to_hf(hf_dataset_name):                                                                                                             #TODO verificar o remove_collumns
-    dataset = dataset.map(prepare_dataset, remove_columns=dataset.column_names["train"], num_proc=1)
-    print(dataset)
-    dataset.save_to_disk(hf_dataset_name)
-    print('Loading_stored_dataset')
-    
-    return dataset
-
-
-def laod_model_from_disk(hf_dataset_name):
-    dataset =  load_from_disk(hf_dataset_name)
-    print(dataset)
-    return dataset
-
-def logout():                                                                #TODO logsout account
-    res = os.system('huggingface-cli logout')
-    print(res)
-
-    
+def logout():         
+    try:
+        res = os.system('huggingface-cli logout')
+        print(res)
+    except:
+        pass
