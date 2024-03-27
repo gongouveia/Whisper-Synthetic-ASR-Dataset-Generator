5a43e07546bb1883e004fbfa46b4ae0884ecbb69
Gonçalo Gouveia <68733294+gongouveia@users.noreply.github.com>
2024-03-27 15:31:48 +0000

    Update main.py config_file param iteration#1

--- main.py
+++ main.py
@@ -21,12 +21,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description="Generate speech with specified project, language, and mode")
     parser.add_argument('--project', type=str, required=True, help='Specify project name')
-    parser.add_argument('--lang', type=str, default='en', choices=['en', 'multi'], help="Specify language ('en' or 'multi')")
+    #parser.add_argument('--lang', type=str, default='en', choices=['en', 'multi'], help="Specify language ('en' or 'multi')")
     parser.add_argument('--theme', type=str, default='light', choices=['auto', 'light', 'dark'], help="Specify theme mode ('auto', 'light', or 'dark')")
     args = parser.parse_args()
-
-
-    write_config_dictionary(args)
 
     if is_path_not_in_universe('projects/'+args.project):
         print('New Project Created.\n')
@@ -35,6 +32,10 @@ if __name__ == '__main__':
         print('Project found. Continue and Visualize Dataset.\n')
         
     print(f'GPU available: {torch.cuda.is_available()}\n')
+
+    write_config_dictionary(args,project, is_gpu_availabel=torch.cuda.is_available())
+
+    
     qdarktheme.enable_hi_dpi()
 
     app = QApplication(sys.argv)
