afde5e59b413d9ece57da8b43dc628c590c8fab2
Gonçalo Gouveia <68733294+gongouveia@users.noreply.github.com>
2024-03-27 15:35:06 +0000

    Update ConfigHandle.py Iteration #1 of main.py

--- Utils/ConfigHandle.py
+++ Utils/ConfigHandle.py
@@ -1,31 +1,25 @@
 import json
 
 
-
-def write_config_dictionary(args):
-    print("Generating speech with the following parameters:")
-    print(f"Project: {args.project}")
-    print(f"Language: {args.lang}")
-    print(f"Mode: {args.theme}")
-    if args.lang == 'en':
-        model = 'medium.en'
-    else: 
-        model = 'medium'
-
-    configs = {'project_name': args.project ,
-               'lang': args.lang,
-               'window_theme': args.theme,
-               'model' : model
-
-               }
-    write_dict_to_json(configs)
-
-
 def write_dict_to_json(dictionary, file_path = 'config.json'):
     with open(file_path, 'w') as json_file:
         json.dump(dictionary, json_file, indent=4)
 
 
+write_config_dictionary(args,project, is_gpu_availabel=torch.cuda.is_available())
+def write_config_dictionary(args):
+    print("Generating speech with the following parameters:")
+    print(f"Project: {args.project}")
+    print(f"Language: {args.lang}")
+    print(f"Mode: {args.theme}")
+
+
+    configs = {'project_name': args.project ,
+               'window_theme': args.theme,
+                'GPU_avail': is_gpu_availabel,
+                'project_path': project
+               }
+    write_dict_to_json(configs)
 
 def retrieve_config_params(file_path = 'config.json'):
     with open(file_path, 'r') as json_file:
